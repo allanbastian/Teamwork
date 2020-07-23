c81c474d08d8c29f279eec306f406d84788b9cfc
Allan Bastian <allan.bastian9.ab@gmail.com>
2020-07-23 17:06:21 +0530

    Added SignUp and SignIn activities

--- app/src/main/java/com/allan/teamwork/MainActivity.java
+++ app/src/main/java/com/allan/teamwork/MainActivity.java
@@ -3,12 +3,17 @@ package com.allan.teamwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button SignUp, SignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignUp = findViewById(R.id.sign_up_btn);
+        SignIn = findViewById(R.id.sign_in_btn);
     }
 }
