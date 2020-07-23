3336462f65edd15d3fcaade87c525f07ab589b85
Allan Bastian <allan.bastian9.ab@gmail.com>
2020-07-23 18:48:02 +0530

    Added API call

--- app/src/main/java/com/allan/teamwork/data/remote/ApiService.java
+++ app/src/main/java/com/allan/teamwork/data/remote/ApiService.java
@@ -1,4 +1,12 @@
 package com.allan.teamwork.data.remote;
 
+import com.allan.teamwork.data.models.PhotoResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiService {
+
+    @GET("photos")
+    Call<PhotoResponse> getPhotos();
 }
--- app/src/main/java/com/allan/teamwork/data/remote/ApiUtils.java
+++ app/src/main/java/com/allan/teamwork/data/remote/ApiUtils.java
@@ -1,12 +1,10 @@
 package com.allan.teamwork.data.remote;
 
-import
-
 public class ApiUtils {
 
     private ApiUtils() {}
 
-    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/photos";
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 
     public static ApiService getApiService(){
         return RetrofitClient.getClient(BASE_URL).create(ApiService.class);
