57031c2449d9dc67d9491ae0fbc40e4529de63d5
Allan Bastian <allan.bastian9.ab@gmail.com>
2020-07-23 18:20:43 +0530

    Finished the contact us fragment

--- app/src/main/java/com/allan/teamwork/ContactUsFragment.java
+++ app/src/main/java/com/allan/teamwork/ContactUsFragment.java
@@ -1,5 +1,7 @@
 package com.allan.teamwork;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +9,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ContactUsFragment extends Fragment {
+
+    private TextView Number, Email;
 
     public ContactUsFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        Number = rootView.findViewById(R.id.mobile_number);
+        Email = rootView.findViewById(R.id.contact_email);
+
+        Number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9876543210"));
+                startActivity(intent);
+            }
+        });
+
+        Email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailClient = new Intent(Intent.ACTION_VIEW);
+                mailClient.setClassName("com.google.android.gm", "com.google.android.gm.ConversationListActivity");
+                startActivity(mailClient);
+            }
+        });
+        return rootView;
     }
 }
